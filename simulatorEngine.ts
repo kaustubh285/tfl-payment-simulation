1101b55b0b9279463c45ab665af107dcb85a7df9
kaustubh285 <kaustubh285@gmail.com>
2023-12-29 18:09:47 +0000

    trainline.json complete

--- simulatorEngine.ts
+++ simulatorEngine.ts
@@ -17,8 +17,6 @@ function SimulationEngine() {
   const Alice = new User("1", "applePay1234");
   const Bob = new User("2", "applePay1234");
   const Julia = new User("3", "applePay1234");
-
-  console.log(Alice.data.id);
 }
 
 module.exports = SimulationEngine;
